17f74ca73bf8fecc41b17077ed36aa774a58cba0
yo <yo@gmail.com>
2019-05-23 20:47:56 -0500

    Urls End points

---
+++ pollsapi/polls/urls.py
@@ -0,0 +1,14 @@
+from django.urls import include, re_path
+from django.urls import path
+#from .views import polls_list, polls_detail
+from .apiviews import PollList, PollDetail
+from .apiviews import ChoiceList, CreateVote
+
+urlpatterns = [
+    path('choices/', ChoiceList.as_view(), name='choice_list'),
+    path('vote/', CreateVote.as_view(), name='create_vote'),
+    path('polls/', PollList.as_view(), name='poll_list'),
+    path('polls/<int:pk>/', PollDetail.as_view(), name='polls_detail'),
+]
+
+
