9ba35ef7b80fe8ee44f834d2c4880b0a9210175e
yo <yo@gmail.com>
2019-05-23 20:48:55 -0500

    Created polls models

--- pollsapi/polls/models.py
+++ pollsapi/polls/models.py
@@ -8,7 +8,6 @@ class Poll(models.Model):
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     pub_date = models.DateTimeField(auto_now=True)
 
-
     def __str__(self):
         return self.question
 
